b0b50af08679c9d5ebd2beccb87ddd2ec16d6492
Tobias Hienzsch <tobias@modern-circuits.com>
2022-09-16 10:12:36 +0200

    Enable Wconversion on gcc & clang

--- cmake/mcCompilerWarnings.cmake
+++ cmake/mcCompilerWarnings.cmake
@@ -14,40 +14,37 @@ else()
 
     target_compile_options(mc_compiler_warnings
         INTERFACE
-        -Wall
-        -Wextra
-        -Wpedantic
-        -Wcast-align
-        -Wshadow
-        -Wunused-parameter
-        -Wnarrowing
-        -Wshadow
+            -Wall
+            -Wextra
+            -Wpedantic
+            -Wcast-align
+            -Wshadow
+            -Wunused-parameter
+            -Wnarrowing
+            -Wshadow
+            -Wconversion
 
         # -Wno-sign-compare
-        # -Wconversion
         $<$<OR:$<CXX_COMPILER_ID:Clang>,$<CXX_COMPILER_ID:AppleClang>>:
-        -Wshadow-all
+            -Wshadow-all
 
-        # -Weverything
-        # -Wno-c++98-compat-pedantic
-        # -Wno-documentation-unknown-command
-        # -Wno-newline-eof
-        # -Wno-float-equal
-        # -Wno-global-constructors
-        # -Wno-padded
-        # -Wno-missing-noreturn
-        # -Wno-disabled-macro-expansion
-        # -Wno-ctad-maybe-unsupported
-        # -Wno-unused-member-function
-        # -Wno-old-style-cast
-        # -Wno-implicit-int-float-conversion
+            # -Weverything
+            # -Wno-c++98-compat-pedantic
+            # -Wno-documentation-unknown-command
+            # -Wno-newline-eof
+            # -Wno-float-equal
+            # -Wno-global-constructors
+            # -Wno-padded
+            # -Wno-missing-noreturn
+            # -Wno-disabled-macro-expansion
+            # -Wno-ctad-maybe-unsupported
+            # -Wno-unused-member-function
+            # -Wno-old-style-cast
+            # -Wno-implicit-int-float-conversion
         >
         $<$<CXX_COMPILER_ID:AppleClang>:
-        -Wno-poison-system-directories
         >
         $<$<CXX_COMPILER_ID:GNU>:
-
-        # -Wlogical-op
         >
     )
 endif(MSVC)
