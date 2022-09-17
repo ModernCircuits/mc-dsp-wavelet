1cbaa06bf9ae97a70e397d95ce7a7f3a94c5f92a
Tobias Hienzsch <tobias@modern-circuits.com>
2022-09-17 19:17:40 +0200

    Refactor coif coefficents loading

--- src/mc/dsp/wavelets/Wavelet.cpp
+++ src/mc/dsp/wavelets/Wavelet.cpp
@@ -14,16 +14,15 @@
 #include <mc/core/memory.hpp>
 #include <mc/core/numbers.hpp>
 #include <mc/core/raise.hpp>
+#include <mc/core/ranges.hpp>
 #include <mc/core/stdexcept.hpp>
 #include <mc/core/string_view.hpp>
 #include <mc/core/utility.hpp>
 
 namespace mc::dsp {
 
-namespace {
-
 template<typename T>
-auto qmfEven(Span<T const> in, T* out)
+static auto qmfEven(Span<T const> in, T* out)
 {
     auto const n = in.size();
     for (std::size_t count = 0; count < n; ++count) {
@@ -33,307 +32,107 @@ auto qmfEven(Span<T const> in, T* out)
 }
 
 template<typename T>
-auto qmfWrev(Span<T const> in, T* out)
+static auto qmfWrev(Span<T const> in, T* out)
 {
     qmfEven(in, out);
     ranges::reverse(Span<T>{out, in.size()});
 }
 
-auto filterLength(char const* name) -> std::size_t
+static auto filterLength(char const* name) -> std::size_t
 {
-    auto nameMatches  = [name](auto const& w) { return w.name == name; };
-    auto const filter = ranges::find_if(allWavelets<float>, nameMatches);
-    if (filter == ranges::end(allWavelets<float>)) {
+    auto const& filters = allWavelets<float>;
+    auto const filter   = ranges::find(filters, name, &WaveletCoefficients<float>::name);
+    if (filter == ranges::end(filters)) {
         raise<InvalidArgument>("wavelet filter not in database");
     }
     return filter->length;
 }
 
-auto fillDauberchies(char const* name, float* lp1, float* hp1, float* lp2, float* hp2)
-    -> size_t
+static auto fillDaubechiesWaveletCoefficients(
+    char const* name,
+    Span<float> lp1,
+    Span<float> hp1,
+    Span<float> lp2,
+    Span<float> hp2
+) -> size_t
 {
     using namespace std::string_view_literals;
 
-    if (name == "haar"sv) { return fillDauberchies("db1", lp1, hp1, lp2, hp2); }
+    if (name == "haar"sv) {
+        return fillDaubechiesWaveletCoefficients("db1", lp1, hp1, lp2, hp2);
+    }
 
-    auto nameMatches  = [name](auto const& w) { return w.name == name; };
-    auto const filter = ranges::find_if(allWavelets<float>, nameMatches);
-    if (filter == ranges::end(allWavelets<float>)) {
+    auto const& filters = allWavelets<float>;
+    auto const filter   = ranges::find(filters, name, &WaveletCoefficients<float>::name);
+    if (filter == ranges::end(filters)) {
         raise<InvalidArgument>("wavelet filter not in database");
     }
 
-    ranges::reverse_copy(filter->coefficients, lp1);
-    qmfWrev(filter->coefficients, hp1);
+    ranges::reverse_copy(filter->coefficients, ranges::begin(lp1));
+    qmfWrev(filter->coefficients, mc::data(hp1));
 
-    ranges::copy(filter->coefficients, lp2);
-    qmfEven(filter->coefficients, hp2);
+    ranges::copy(filter->coefficients, ranges::begin(lp2));
+    qmfEven(filter->coefficients, mc::data(hp2));
 
     return filter->length;
 }
 
-auto waveletFilterCoefficients(
+static auto fillCoifWaveletCoefficients(
     char const* name,
-    float* lp1,
-    float* hp1,
-    float* lp2,
-    float* hp2
-) -> std::size_t
+    Span<float> lp1,
+    Span<float> hp1,
+    Span<float> lp2,
+    Span<float> hp2
+) -> size_t
 {
-    auto const n        = filterLength(name);
-    auto const nameView = StringView{name};
-    if (nameView.find("haar") != StringView::npos) {
-        return fillDauberchies(name, lp1, hp1, lp2, hp2);
-    }
-    if (nameView.find("db") != StringView::npos) {
-        return fillDauberchies(name, lp1, hp1, lp2, hp2);
-    }
-    if (nameView.find("sym") != StringView::npos) {
-        return fillDauberchies(name, lp1, hp1, lp2, hp2);
+    auto const& filters = coifWavelets<float>;
+    auto const filter   = ranges::find(filters, name, &WaveletCoefficients<float>::name);
+    if (filter == ranges::end(filters)) {
+        raise<InvalidArgument>("wavelet filter not in database");
     }
 
-    if (name == StringView{"coif1"}) {
-        auto coeffTemp = makeUnique<float[]>(n);
+    auto scale  = [](auto c) { return c * static_cast<float>(numbers::sqrt2); };
+    auto scaled = filter->coefficients | ranges::views::transform(scale);
 
-        std::copy(cbegin(coif1<float>), cend(coif1<float>), coeffTemp.get());
-        for (auto i = 0; i < n; ++i) { coeffTemp[i] *= static_cast<float>(numbers::sqrt2); }
+    ranges::copy(scaled, ranges::begin(lp2));
+    ranges::reverse_copy(scaled, ranges::begin(lp1));
+    qmfWrev<float>(lp2, mc::data(hp1));
+    qmfEven<float>(lp2, mc::data(hp2));
 
-        std::reverse_copy(coeffTemp.get(), coeffTemp.get() + n, lp1);
-        qmfWrev({coeffTemp.get(), n}, hp1);
-        std::copy(coeffTemp.get(), coeffTemp.get() + n, lp2);
-        qmfEven({coeffTemp.get(), n}, hp2);
-
-        return n;
-    }
-
-    if (name == StringView{"coif2"}) {
-        auto coeffTemp = makeUnique<float[]>(n);
-
-        std::copy(cbegin(coif2<float>), cend(coif2<float>), coeffTemp.get());
-        for (auto i = 0; i < n; ++i) { coeffTemp[i] *= static_cast<float>(numbers::sqrt2); }
-
-        std::reverse_copy(coeffTemp.get(), coeffTemp.get() + n, lp1);
-        qmfWrev({coeffTemp.get(), n}, hp1);
-        std::copy(coeffTemp.get(), coeffTemp.get() + n, lp2);
-        qmfEven({coeffTemp.get(), n}, hp2);
-
-        return n;
-    }
-
-    if (name == StringView{"coif3"}) {
-        auto coeffTemp = makeUnique<float[]>(n);
-
-        std::copy(cbegin(coif3<float>), cend(coif3<float>), coeffTemp.get());
-        for (auto i = 0; i < n; ++i) { coeffTemp[i] *= static_cast<float>(numbers::sqrt2); }
-
-        std::reverse_copy(coeffTemp.get(), coeffTemp.get() + n, lp1);
-        qmfWrev({coeffTemp.get(), n}, hp1);
-        std::copy(coeffTemp.get(), coeffTemp.get() + n, lp2);
-        qmfEven({coeffTemp.get(), n}, hp2);
-
-        return n;
-    }
-
-    if (name == StringView{"coif4"}) {
-        auto coeffTemp = makeUnique<float[]>(n);
-
-        std::copy(cbegin(coif4<float>), cend(coif4<float>), coeffTemp.get());
-        for (auto i = 0; i < n; ++i) { coeffTemp[i] *= static_cast<float>(numbers::sqrt2); }
-
-        std::reverse_copy(coeffTemp.get(), coeffTemp.get() + n, lp1);
-        qmfWrev({coeffTemp.get(), n}, hp1);
-        std::copy(coeffTemp.get(), coeffTemp.get() + n, lp2);
-        qmfEven({coeffTemp.get(), n}, hp2);
-
-        return n;
-    }
-
-    if (name == StringView{"coif5"}) {
-        auto coeffTemp = makeUnique<float[]>(n);
-
-        std::copy(cbegin(coif5<float>), cend(coif5<float>), coeffTemp.get());
-        for (auto i = 0; i < n; ++i) { coeffTemp[i] *= static_cast<float>(numbers::sqrt2); }
-
-        std::reverse_copy(coeffTemp.get(), coeffTemp.get() + n, lp1);
-        qmfWrev({coeffTemp.get(), n}, hp1);
-        std::copy(coeffTemp.get(), coeffTemp.get() + n, lp2);
-        qmfEven({coeffTemp.get(), n}, hp2);
-
-        return n;
-    }
-
-    if (name == StringView{"coif6"}) {
-        auto coeffTemp = makeUnique<float[]>(n);
-
-        std::copy(cbegin(coif6<float>), cend(coif6<float>), coeffTemp.get());
-        for (auto i = 0; i < n; ++i) { coeffTemp[i] *= static_cast<float>(numbers::sqrt2); }
-
-        std::reverse_copy(coeffTemp.get(), coeffTemp.get() + n, lp1);
-        qmfWrev({coeffTemp.get(), n}, hp1);
-        std::copy(coeffTemp.get(), coeffTemp.get() + n, lp2);
-        qmfEven({coeffTemp.get(), n}, hp2);
-
-        return n;
-    }
-
-    if (name == StringView{"coif7"}) {
-        auto coeffTemp = makeUnique<float[]>(n);
-
-        std::copy(cbegin(coif7<float>), cend(coif7<float>), coeffTemp.get());
-        for (auto i = 0; i < n; ++i) { coeffTemp[i] *= static_cast<float>(numbers::sqrt2); }
-
-        std::reverse_copy(coeffTemp.get(), coeffTemp.get() + n, lp1);
-        qmfWrev({coeffTemp.get(), n}, hp1);
-        std::copy(coeffTemp.get(), coeffTemp.get() + n, lp2);
-        qmfEven({coeffTemp.get(), n}, hp2);
-
-        return n;
-    }
-
-    if (name == StringView{"coif8"}) {
-        auto coeffTemp = makeUnique<float[]>(n);
-
-        std::copy(cbegin(coif8<float>), cend(coif8<float>), coeffTemp.get());
-        for (auto i = 0; i < n; ++i) { coeffTemp[i] *= static_cast<float>(numbers::sqrt2); }
-
-        std::reverse_copy(coeffTemp.get(), coeffTemp.get() + n, lp1);
-        qmfWrev({coeffTemp.get(), n}, hp1);
-        std::copy(coeffTemp.get(), coeffTemp.get() + n, lp2);
-        qmfEven({coeffTemp.get(), n}, hp2);
-
-        return n;
-    }
-
-    if (name == StringView{"coif9"}) {
-        auto coeffTemp = makeUnique<float[]>(n);
-
-        std::copy(cbegin(coif9<float>), cend(coif9<float>), coeffTemp.get());
-        for (auto i = 0; i < n; ++i) { coeffTemp[i] *= static_cast<float>(numbers::sqrt2); }
-
-        std::reverse_copy(coeffTemp.get(), coeffTemp.get() + n, lp1);
-        qmfWrev({coeffTemp.get(), n}, hp1);
-        std::copy(coeffTemp.get(), coeffTemp.get() + n, lp2);
-        qmfEven({coeffTemp.get(), n}, hp2);
-
-        return n;
-    }
-
-    if (name == StringView{"coif10"}) {
-        auto coeffTemp = makeUnique<float[]>(n);
-
-        std::copy(cbegin(coif10<float>), cend(coif10<float>), coeffTemp.get());
-        for (auto i = 0; i < n; ++i) { coeffTemp[i] *= static_cast<float>(numbers::sqrt2); }
-
-        std::reverse_copy(coeffTemp.get(), coeffTemp.get() + n, lp1);
-        qmfWrev({coeffTemp.get(), n}, hp1);
-        std::copy(coeffTemp.get(), coeffTemp.get() + n, lp2);
-        qmfEven({coeffTemp.get(), n}, hp2);
-
-        return n;
-    }
-    if (name == StringView{"coif11"}) {
-        auto coeffTemp = makeUnique<float[]>(n);
-
-        std::copy(cbegin(coif11<float>), cend(coif11<float>), coeffTemp.get());
-        for (auto i = 0; i < n; ++i) { coeffTemp[i] *= static_cast<float>(numbers::sqrt2); }
-
-        std::reverse_copy(coeffTemp.get(), coeffTemp.get() + n, lp1);
-        qmfWrev({coeffTemp.get(), n}, hp1);
-        std::copy(coeffTemp.get(), coeffTemp.get() + n, lp2);
-        qmfEven({coeffTemp.get(), n}, hp2);
-
-        return n;
-    }
-    if (name == StringView{"coif12"}) {
-        auto coeffTemp = makeUnique<float[]>(n);
-
-        std::copy(cbegin(coif12<float>), cend(coif12<float>), coeffTemp.get());
-        for (auto i = 0; i < n; ++i) { coeffTemp[i] *= static_cast<float>(numbers::sqrt2); }
-
-        std::reverse_copy(coeffTemp.get(), coeffTemp.get() + n, lp1);
-        qmfWrev({coeffTemp.get(), n}, hp1);
-        std::copy(coeffTemp.get(), coeffTemp.get() + n, lp2);
-        qmfEven({coeffTemp.get(), n}, hp2);
-
-        return n;
-    }
-    if (name == StringView{"coif13"}) {
-        auto coeffTemp = makeUnique<float[]>(n);
-
-        std::copy(cbegin(coif13<float>), cend(coif13<float>), coeffTemp.get());
-        for (auto i = 0; i < n; ++i) { coeffTemp[i] *= static_cast<float>(numbers::sqrt2); }
-
-        std::reverse_copy(coeffTemp.get(), coeffTemp.get() + n, lp1);
-        qmfWrev({coeffTemp.get(), n}, hp1);
-        std::copy(coeffTemp.get(), coeffTemp.get() + n, lp2);
-        qmfEven({coeffTemp.get(), n}, hp2);
-
-        return n;
-    }
-    if (name == StringView{"coif14"}) {
-        auto coeffTemp = makeUnique<float[]>(n);
-
-        std::copy(cbegin(coif14<float>), cend(coif14<float>), coeffTemp.get());
-        for (auto i = 0; i < n; ++i) { coeffTemp[i] *= static_cast<float>(numbers::sqrt2); }
-
-        std::reverse_copy(coeffTemp.get(), coeffTemp.get() + n, lp1);
-        qmfWrev({coeffTemp.get(), n}, hp1);
-        std::copy(coeffTemp.get(), coeffTemp.get() + n, lp2);
-        qmfEven({coeffTemp.get(), n}, hp2);
-
-        return n;
-    }
-    if (name == StringView{"coif15"}) {
-        auto coeffTemp = makeUnique<float[]>(n);
-
-        std::copy(cbegin(coif15<float>), cend(coif15<float>), coeffTemp.get());
-        for (auto i = 0; i < n; ++i) { coeffTemp[i] *= static_cast<float>(numbers::sqrt2); }
-
-        std::reverse_copy(coeffTemp.get(), coeffTemp.get() + n, lp1);
-        qmfWrev({coeffTemp.get(), n}, hp1);
-        std::copy(coeffTemp.get(), coeffTemp.get() + n, lp2);
-        qmfEven({coeffTemp.get(), n}, hp2);
-
-        return n;
-    }
-    if (name == StringView{"coif16"}) {
-        auto coeffTemp = makeUnique<float[]>(n);
-
-        std::copy(cbegin(coif16<float>), cend(coif16<float>), coeffTemp.get());
-        for (auto i = 0; i < n; ++i) { coeffTemp[i] *= static_cast<float>(numbers::sqrt2); }
-
-        std::reverse_copy(coeffTemp.get(), coeffTemp.get() + n, lp1);
-        qmfWrev({coeffTemp.get(), n}, hp1);
-        std::copy(coeffTemp.get(), coeffTemp.get() + n, lp2);
-        qmfEven({coeffTemp.get(), n}, hp2);
-
-        return n;
-    }
-    if (name == StringView{"coif17"}) {
-        auto coeffTemp = makeUnique<float[]>(n);
-
-        std::copy(cbegin(coif17<float>), cend(coif17<float>), coeffTemp.get());
-        for (auto i = 0; i < n; ++i) { coeffTemp[i] *= static_cast<float>(numbers::sqrt2); }
-
-        std::reverse_copy(coeffTemp.get(), coeffTemp.get() + n, lp1);
-        qmfWrev({coeffTemp.get(), n}, hp1);
-        std::copy(coeffTemp.get(), coeffTemp.get() + n, lp2);
-        qmfEven({coeffTemp.get(), n}, hp2);
-
-        return n;
-    }
-
-    print("\n Filter Not in Database \n");
-    return -1;
+    return filter->length;
 }
 
-}  // namespace
+static auto fillWaveletFilterCoefficients(
+    char const* name,
+    Span<float> lp1,
+    Span<float> hp1,
+    Span<float> lp2,
+    Span<float> hp2
+) -> std::size_t
+{
+    if (StringView{name}.find("haar") != StringView::npos) {
+        return fillDaubechiesWaveletCoefficients(name, lp1, hp1, lp2, hp2);
+    }
+    if (StringView{name}.find("db") != StringView::npos) {
+        return fillDaubechiesWaveletCoefficients(name, lp1, hp1, lp2, hp2);
+    }
+    if (StringView{name}.find("sym") != StringView::npos) {
+        return fillDaubechiesWaveletCoefficients(name, lp1, hp1, lp2, hp2);
+    }
+    if (StringView{name}.find("coif") != StringView::npos) {
+        return fillCoifWaveletCoefficients(name, lp1, hp1, lp2, hp2);
+    }
+
+    raisef<InvalidArgument>("filter not in database: {}", name);
+    return -1;
+}
 
 Wavelet::Wavelet(char const* name)
     : _name{name}
     , _size{static_cast<std::size_t>(filterLength(name))}
     , _params{makeUnique<float[]>(4 * _size)}
 {
-    waveletFilterCoefficients(name, data(lpd()), data(hpd()), data(lpr()), data(hpr()));
+    fillWaveletFilterCoefficients(name, lpd(), hpd(), lpr(), hpr());
 }
 
 auto Wavelet::size() const noexcept -> std::size_t { return _size; }
