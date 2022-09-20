93a1b9f2ab786ebe870b5d6fb7e9bef4fc6b28a7
Tobias Hienzsch <tobias@modern-circuits.com>
2022-09-20 07:14:12 +0200

    [fft] Add PFFFT_Handle & PFFFT_Deleter

--- src/mc/dsp/fft/backend/PFFFT.cpp
+++ src/mc/dsp/fft/backend/PFFFT.cpp
@@ -10,68 +10,33 @@
 namespace mc::dsp {
 
 PFFFT_Complex_Float::PFFFT_Complex_Float(std::size_t size)
-    : _setup{pffft_new_setup(size, PFFFT_COMPLEX)}
+    : _setup{PFFFT_Handle{pffft_new_setup(static_cast<int>(size), PFFFT_COMPLEX)}}
 {}
-
-PFFFT_Complex_Float::~PFFFT_Complex_Float()
-{
-    if (_setup != nullptr) { pffft_destroy_setup(_setup); }
-}
-
-PFFFT_Complex_Float::PFFFT_Complex_Float(PFFFT_Complex_Float&& other) noexcept
-    : _setup{std::exchange(other._setup, nullptr)}
-{}
-
-auto PFFFT_Complex_Float::operator=(PFFFT_Complex_Float&& other) noexcept
-    -> PFFFT_Complex_Float&
-{
-    _setup = std::exchange(other._setup, nullptr);
-    return *this;
-}
 
 auto PFFFT_Complex_Float::fft(Complex<float> const* in, Complex<float>* out) -> void
 {
     auto const* input = reinterpret_cast<float const*>(in);  // NOLINT
     auto* output      = reinterpret_cast<float*>(out);       // NOLINT
-    pffft_transform_ordered(_setup, input, output, nullptr, PFFFT_FORWARD);
+    pffft_transform_ordered(_setup.get(), input, output, nullptr, PFFFT_FORWARD);
 }
 
 auto PFFFT_Complex_Float::ifft(Complex<float> const* in, Complex<float>* out) -> void
 {
     auto const* input = reinterpret_cast<float const*>(in);  // NOLINT
     auto* output      = reinterpret_cast<float*>(out);       // NOLINT
-    pffft_transform_ordered(_setup, input, output, nullptr, PFFFT_BACKWARD);
+    pffft_transform_ordered(_setup.get(), input, output, nullptr, PFFFT_BACKWARD);
 }
 
 PFFFT_Real_Float::PFFFT_Real_Float(size_t n)
     : _n{static_cast<int>(n)}
-    , _setup{pffft_new_setup(static_cast<int>(n), PFFFT_REAL)}
+    , _setup{PFFFT_Handle{pffft_new_setup(static_cast<int>(n), PFFFT_REAL)}}
 {
     _tmp.resize(n);
 }
 
-PFFFT_Real_Float::~PFFFT_Real_Float()
-{
-    if (_setup != nullptr) { pffft_destroy_setup(_setup); }
-}
-
-PFFFT_Real_Float::PFFFT_Real_Float(PFFFT_Real_Float&& other) noexcept
-    : _n{std::exchange(other._n, 0)}
-    , _setup{std::exchange(other._setup, nullptr)}
-    , _tmp{std::exchange(other._tmp, {})}
-{}
-
-auto PFFFT_Real_Float::operator=(PFFFT_Real_Float&& other) noexcept -> PFFFT_Real_Float&
-{
-    _n     = std::exchange(other._n, 0);
-    _setup = std::exchange(other._setup, nullptr);
-    _tmp   = std::exchange(other._tmp, {});
-    return *this;
-}
-
 auto PFFFT_Real_Float::rfft(float const* inp, Complex<float>* oup) -> void
 {
-    pffft_transform_ordered(_setup, inp, (float*)oup, nullptr, PFFFT_FORWARD);
+    pffft_transform_ordered(_setup.get(), inp, (float*)oup, nullptr, PFFFT_FORWARD);
 
     // Move compressed DC/Nyquist components to correct location
     auto const h = _n / 2;
@@ -89,7 +54,7 @@ auto PFFFT_Real_Float::irfft(Complex<float> const* inp, float* oup) -> void
     _tmp[0] = {_tmp[0].real(), _tmp[_n / 2].real()};
 
     auto const* in = reinterpret_cast<float const*>(_tmp.data());
-    pffft_transform_ordered(_setup, in, oup, nullptr, PFFFT_BACKWARD);
+    pffft_transform_ordered(_setup.get(), in, oup, nullptr, PFFFT_BACKWARD);
 }
 
 }  // namespace mc::dsp
--- src/mc/dsp/fft/backend/PFFFT.hpp
+++ src/mc/dsp/fft/backend/PFFFT.hpp
@@ -3,47 +3,41 @@
 #pragma once
 
 #include <mc/core/complex.hpp>
+#include <mc/core/memory.hpp>
 #include <mc/core/vector.hpp>
 
 #include <pffft.h>
 
 namespace mc::dsp {
 
+struct PFFFT_Deleter
+{
+    auto operator()(PFFFT_Setup* setup) { pffft_destroy_setup(setup); }
+};
+
+using PFFFT_Handle = UniquePtr<PFFFT_Setup, PFFFT_Deleter>;
+
 struct PFFFT_Complex_Float
 {
     explicit PFFFT_Complex_Float(std::size_t size);
-    ~PFFFT_Complex_Float();
-
-    PFFFT_Complex_Float(PFFFT_Complex_Float const& other)                    = delete;
-    auto operator=(PFFFT_Complex_Float const& other) -> PFFFT_Complex_Float& = delete;
-
-    PFFFT_Complex_Float(PFFFT_Complex_Float&& other) noexcept;
-    auto operator=(PFFFT_Complex_Float&& other) noexcept -> PFFFT_Complex_Float&;
 
     auto fft(Complex<float> const* in, Complex<float>* out) -> void;
     auto ifft(Complex<float> const* in, Complex<float>* out) -> void;
 
 private:
-    PFFFT_Setup* _setup;
+    PFFFT_Handle _setup;
 };
 
 struct PFFFT_Real_Float
 {
     explicit PFFFT_Real_Float(size_t n);
-    ~PFFFT_Real_Float();
-
-    PFFFT_Real_Float(PFFFT_Real_Float const& other)                    = delete;
-    auto operator=(PFFFT_Real_Float const& other) -> PFFFT_Real_Float& = delete;
-
-    PFFFT_Real_Float(PFFFT_Real_Float&& other) noexcept;
-    auto operator=(PFFFT_Real_Float&& other) noexcept -> PFFFT_Real_Float&;
 
     auto rfft(float const* inp, Complex<float>* oup) -> void;
     auto irfft(Complex<float> const* inp, float* oup) -> void;
 
 private:
     int _n;
-    PFFFT_Setup* _setup;
+    PFFFT_Handle _setup;
 
     // We need to rearrange the inverse transform input
     // and it is passed as const. So we make a copy and modify.
